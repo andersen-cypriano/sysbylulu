5c68dd6257fdd86951f3fb1a982c59e04cc08b60
Andersen Cypriano <acypriano@mtsolucoes.com.br>
2023-11-13 18:05:08 -0300

    style: header desktop

--- header/script.js
+++ header/script.js
@@ -3,14 +3,15 @@ const cabecalho = {
     const contentHeader = document.querySelector('#cabecalho');
     const contentTopBar = document.createElement('div');
     contentTopBar.classList.add('content-top-bar')
+    
     const contentCenterTopBar = document.createElement('div');
-    contentCenterTopBar.classList.add('content-center')
+    contentCenterTopBar.classList.add('content-center');
     
     const alertCupom = document.createElement('p');
     alertCupom.innerHTML = `COMPRE COM O CUPOM <span>#PRIMEIRACOMPRA</span> E <span>GANHE 10% OFF</span>`;
 
     const contactButtonTopBar = document.createElement('a');
-    contactButtonTopBar.innerHTML = `<span>#</span> Fale Conosco`;
+    contactButtonTopBar.innerHTML = `<span></span> Fale Conosco`;
 
     contentCenterTopBar.appendChild(alertCupom);
     contentCenterTopBar.appendChild(contactButtonTopBar);
@@ -18,8 +19,45 @@ const cabecalho = {
 
     contentHeader.prepend(contentTopBar);
   },
+  createContentSearch: ()=> {
+    const btnMiniMenu = document.createElement('div');
+    btnMiniMenu.className = 'btn-mini-menu';
+    btnMiniMenu.addEventListener('click', () => {
+      document.querySelector('#cabecalho .menu').classList.toggle('menu-hidden');
+    })
+    document.querySelector('.conteudo-topo').prepend(btnMiniMenu)
+    document.querySelector('.conteudo-topo').appendChild(document.querySelector('#form-buscar'));
+  },
+  miniheader: ()=> {
+    const cabecalho = document.querySelector('#cabecalho');
+    function setMiniHeader () {
+      cabecalho.classList.add('mini-header');
+      document.querySelector('#cabecalho .menu').classList.add('menu-hidden');
+    }
+    function removeMiniHeader () {
+      cabecalho.classList.remove('mini-header');
+      document.querySelector('#cabecalho .menu').classList.aremove('menu-hidden');
+    }
+    window.scrollY > 0 ?  setMiniHeader () :  removeMiniHeader (); 
+    
+  },
+  addButtonsMiniHeader: () => {
+    // button contact
+    const buttonContact = document.querySelector('.content-top-bar a').cloneNode(true);
+    buttonContact.classList.add('btn-contact');
+
+    // btn mini header
+    document.querySelector('.conteudo-actions').appendChild(buttonContact);
+  },
   init: function () {
     this.createTopBar();
+    this.createContentSearch();
+    this.addButtonsMiniHeader();
+
+    window.addEventListener('scroll', ()=> {
+      window.matchMedia("(min-width: 769px)").matches ? this.miniheader() : this.removeminiheader();
+      
+    });
   }
 }
 
