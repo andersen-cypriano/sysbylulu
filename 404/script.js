7b77f5cf05acedda2bb70b7fb104684fd3a9a769
Andersen Cypriano <acypriano@mtsolucoes.com.br>
2023-12-06 17:36:42 -0300

    revisao v1.0

--- 404/script.js
+++ 404/script.js
@@ -9,6 +9,6 @@ const errorPage = {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(".box-destaque .titulo").textContent == 'Página não encontrada' ? errorPage.init() : null;
+  document.querySelector(".box-destaque") && document.querySelector(".box-destaque .titulo").textContent == 'Página não encontrada' ? errorPage.init() : null;
 
 });
