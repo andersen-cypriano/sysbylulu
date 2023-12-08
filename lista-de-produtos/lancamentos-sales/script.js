5d1d25b1454fd5e77627e6c7cc97fbd935bc7f39
Andersen Cypriano <acypriano@mtsolucoes.com.br>
2023-12-08 13:43:51 -0300

    revisao v1.3

--- lista-de-produtos/lancamentos-sales/script.js
+++ lista-de-produtos/lancamentos-sales/script.js
@@ -28,7 +28,6 @@ const productListFeatures = {
       animation: "slide",
       animationLoop: false,
       itemWidth: 210,
-      itemMargin: 5,
       minItems: 2,
       maxItems: 4
     });
--- lista-de-produtos/ultima-chance/script.js
+++ lista-de-produtos/ultima-chance/script.js
@@ -28,7 +28,6 @@ const ultimaChance = {
       animation: "slide",
       animationLoop: false,
       itemWidth: 210,
-      itemMargin: 5,
       minItems: 2,
       maxItems: 2
     });
